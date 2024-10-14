e02c215e9eeb9036e93ec73b5bce478ad12e9d1e
geisonhg <67343209+geisonhg@users.noreply.github.com>
2024-10-14 14:41:56 +0100

    Add project files.

---
+++ 20.14.24_Interface/Models/Models.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._14._24_Interface.Models
+{
+    interface OperateVehicle
+    {
+        void Operate();//Interface method doestnt have a body
+
+    }
+
+    interface accesibleTransport
+    {
+        Boolean isPublicAccesible(); 
+    }
+
+    interface Flyiable : OperateVehicle
+    {
+        void Fly();
+    }
+
+    interface Drivable : OperateVehicle
+    {
+        void Drive();
+    }
+
+    interface Rideable : OperateVehicle
+    {
+        void Ride();
+    }
+
+
+    class Airplane : Flyiable,accesibleTransport
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Flying...");
+        }
+
+        public void Operate()
+        {
+            Fly();
+        }
+
+        public bool isPublicAccesible()
+        {
+            return true;
+        }
+    }
+
+    class Bike : Rideable, accesibleTransport
+    {
+        public void Ride()
+        {
+            Console.WriteLine("Riding...");
+        }
+
+        public void Operate()
+        {
+            Ride();
+        }
+
+        public bool isPublicAccesible()
+        {
+            return false;
+        }
+    }
+
+    public class Truck : Drivable, accesibleTransport 
+    {
+        public bool isPublicAccesible() 
+        {
+            return false ;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving...");
+        }
+
+        public void Operate()
+        {
+            Drive();
+        }
+
+    }
+
+}
---
+++ 20.14.24_Interface/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _20._14._24_Interface.Models;
+
+Console.WriteLine("Hello, World!");
+
+Bike bike = new Bike();
+bike.Ride();
